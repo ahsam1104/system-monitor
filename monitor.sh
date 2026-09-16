5732aabf2376213b0a36bfc1349addbd443684ea
ahsam <sam@nyx.nyx.org>
2026-09-17 01:52:21 +0500

    Add memory monitoring

--- monitor.sh
+++ monitor.sh
@@ -23,3 +23,17 @@ echo "Cores: $CPU_CORES"
 CPU_USAGE=$(top -bn1| grep "Cpu(s)"| awk '{print 100 -$8}')
 
 echo "Usage: ${CPU_USAGE}%"
+
+echo 
+echo "- - - - - - - MEMORY- - - - - -"
+
+TOTAL_RAM=$(free -m |awk '/Mem:/ {print $2}')
+USED_RAM=$(free -m |awk '/Mem:/ {print $3}')
+AVAILABLE_RAM=$(free -m|awk '/Mem:/ {print $7}')
+
+RAM_USAGE=$((USED_RAM * 100 / TOTAL_RAM))
+
+echo "Total: ${TOTAL_RAM}MB"
+echo "Used: ${USED_RAM}MB"
+echo "Available: ${AVAILABLE_RAM}MB"
+echo "Usage: ${RAM_USAGE}%"
