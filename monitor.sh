b4a386288165c905877ff6b3e8363ebb32cc6ca7
ahsam <sam@nyx.nyx.org>
2026-09-17 01:37:55 +0500

    Add CPU information

--- monitor.sh
+++ monitor.sh
@@ -8,3 +8,14 @@ echo "Hostname: $(hostname)"
 echo "kernel: $(uname -r)"
 echo "Uptime: $(uptime -p)"
 
+
+echo 
+
+echo "- - - - - - - CPU- - - - - - - - "
+
+CPU_MODEL=$(lscpu | grep "Model name:"|sed 's/Model name:[[:space:]]*//')
+CPU_CORES=$(nproc)
+
+echo "Model: $CPU_MODEL"
+
+echo "Cores: $CPU_CORES"
